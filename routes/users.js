9c4bc34658c0276e0de43bb014c8f47fe19760c5
Aedan <aedan.giffin@gmail.com>
2019-02-01 22:12:58 +0000

    Add the rest of the routes for /your-lists/

--- routes/users.js
+++ routes/users.js
@@ -19,5 +19,58 @@ module.exports = (knex) => {
       })
   });
 
+  //insert new to-do list (FOR USER_ID 1 CURRENTLY)
+  userRoutes.put('/new-item', (request, response) => {
+    knex('to_dos')
+    .insert([{
+      to_do: `${item}`, //change to req.body.item or whatever it is
+      user_id: "1", //should be from req.body (not hardcoded)
+      cat_id: `${cat}`, //same as item
+      priority: "false"
+    }])
+    .then( () => {
+      console.log("insert complete!");
+    })
+    .catch( (error) => {
+      console.error(error);
+    });  
+  });
+
+  userRoutes.patch('/recat-item', (request, response) => { 
+    knex('to_dos')
+    .where({id: 8}) //id should be id of the item being recategorized
+    .update({ cat_id: `${cat}` }) //cat should be id of new category chosen (or done)
+    .then( () => {
+      console.log("Insert complete!");
+    })
+    .catch( (error) => {
+      console.error(error);
+    });
+  });
+
+  userRoutes.patch('/prioritize-item', (request, response) => { 
+    knex('to_dos')
+    .where({id: 8}) //id should be id of the item being prioritized
+    .update({ priority: "true" }) //priority should be set to true or false (determined before ajax req)
+    .then( () => {
+      console.log("Update complete!");
+    })
+    .catch( (error) => {
+      console.error(error);
+    });
+  });
+
+  userRoutes.delete('/delete-item', (request, response) => { 
+    knex('to_dos')
+    .where({id: 8}) //id should be id of the item being deleted
+    .del() 
+    .then( () => {
+      console.log("Item deleted!");
+    })
+    .catch( (error) => {
+      console.error(error);
+    });
+  });
+
   return userRoutes;
 }
