8da626a8aa9ee8fc6f2c8d59dc4f8d845b7dd45f
Aedan <aedan.giffin@gmail.com>
2019-02-02 00:55:32 +0000

    Add function to render list items in appropriate lists (by category, priority)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,28 @@
 $(() => {
+  const catLookup = {
+    '1': 'read',
+    '2': 'eat',
+    '3': 'buy',
+    '4': 'watch',
+    '5': 'done'
+  };
+
   $.ajax({
-    method: "GET",
-    url: "/your-lists"
+    method: 'GET',
+    url: '/your-lists'
   }).done((rows) => {
     for(row of rows) {
-      $("<div>").text(row.to_do + ", category: " + row.category).appendTo($("body"));
+      let thisClass = row.category;
+      if (!thisClass) {
+        thisClass = 'uncat';
+      }
+      if (row.priority) {
+        $('<span>').text(row.to_do).addClass(`${thisClass} priority list-item`).prependTo(`div#${thisClass}`);
+      } else {
+        $('<span>').text(row.to_do).addClass(`${thisClass} list-item`).appendTo(`div#${thisClass}`);
+      }
     }
   });
 });
+
+
