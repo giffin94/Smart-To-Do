5f3c097a0434342becc5d6a548b950ca710c4a4b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-03 04:48:08 +0000

    Add indent to jQuery for readability.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,16 +11,16 @@ $(() => {
       }
       if (row.priority) {
         $('<div>').addClass('list-item')
-        .append($('<span>').addClass('check').text('√'))
-        .append($('<span>').addClass(`${thisClass} priority list-text`).text(row.to_do))
-        .append($('<span>').addClass('options').text('&'))
-        .prependTo(`div#${thisClass}`);
+          .append($('<span>').addClass('check').text('√'))
+          .append($('<span>').addClass(`${thisClass} priority list-text`).text(row.to_do))
+          .append($('<span>').addClass('options').text('&'))
+          .prependTo(`div#${thisClass}`);
       } else {
         $('<div>').addClass('list-item')
-        .append($('<span>').addClass('check').text('√'))
-        .append($('<span>').addClass(`${thisClass} list-text`).text(row.to_do))
-        .append($('<span>').addClass('options').text('&'))
-        .appendTo(`div#${thisClass}`);
+          .append($('<span>').addClass('check').text('√'))
+          .append($('<span>').addClass(`${thisClass} list-text`).text(row.to_do))
+          .append($('<span>').addClass('options').text('&'))
+          .appendTo(`div#${thisClass}`);
       }
     }
   }
