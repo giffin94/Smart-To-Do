d3385e30a24e8c6ab774582487186cd21c1f6a6b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-04 07:17:27 +0000

    Create elements for flexbox. Append jQ properly.

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,20 +1,21 @@
 $(() => {
 
   function renderUserInfo(data) {
-    const userName = data[0].name
-    const userEmail = data[0].email
+    const userName = data[0].name;
+    const userEmail = data[0].email;
+
     $('<div>').addClass('profile-data')
       .append($('<div>').addClass('username').text(`Username: ${userName}`))
       .append($('<div>').addClass('email').text(`Email: ${userEmail}`))
       .append($('<button>').addClass('edit').text('edit'))
-      .appendTo('.profile-holder');
+      .appendTo('.profile-flexbox');
   }
 
   $('<form>').addClass('profile-form')
     .append($('<input type="text" name="name" placeholder="your name">').addClass('input'))
     .append($('<input type="text" name="email" placeholder="your email">').addClass('input'))
     .append($('<input type="submit" value="Update Profile">').addClass('button'))
-    .appendTo('.profile-holder');
+    .appendTo('.profile-flexbox');
 
 
   $.ajax({
