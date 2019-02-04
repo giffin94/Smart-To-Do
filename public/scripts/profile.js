b1b0d92e43ef9f4a4d11a4724027d932419a7b72
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-04 07:27:01 +0000

    Add event to edit button to hide/reveal form and user data.

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -11,7 +11,7 @@ $(() => {
       .appendTo('.profile-flexbox');
   }
 
-  $('<form>').addClass('profile-form')
+  $('<form>').addClass('profile-form hidden')
     .append($('<input type="text" name="name" placeholder="your name">').addClass('input'))
     .append($('<input type="text" name="email" placeholder="your email">').addClass('input'))
     .append($('<input type="submit" value="Update Profile">').addClass('button'))
@@ -23,6 +23,13 @@ $(() => {
     url: '/profile/info'
   }).then( (rows) => {
     renderUserInfo(rows);
+    makeEvents();
   })
 
+  function makeEvents() {
+    $('button.edit').on('click', function() {
+      $(this).parents('.profile-data').toggle().toggleClass('hidden');
+      $(this).parent().prev('.profile-form').toggle().toggleClass('hidden');
+    });
+  }
 });
